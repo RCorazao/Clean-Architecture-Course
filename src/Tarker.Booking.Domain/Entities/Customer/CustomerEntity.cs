fca9be0c39bc34665ba9047fdfe80e4db305c034
ReisonC <128325868+RCorazao@users.noreply.github.com>
2024-03-17 16:31:34 -0500

    Add DB Packages

---
+++ src/Tarker.Booking.Domain/Entities/Customer/CustomerEntity.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Tarker.Booking.Domain.Entities.Customer
+{
+    public class CustomerEntity
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string DocumentNumber { get; set; }
+    }
+}
