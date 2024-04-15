4b52eba29d3eb47da885b5b8551a46602fb14a49
ReisonC <128325868+RCorazao@users.noreply.github.com>
2024-04-14 19:08:38 -0500

    Add User APIs

--- src/Tarker.Booking.Api/Controllers/UserController.cs
+++ src/Tarker.Booking.Api/Controllers/UserController.cs
@@ -1,5 +1,12 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
+using Tarker.Booking.Application.DataBase.User.Commands.CreateUser;
+using Tarker.Booking.Application.DataBase.User.Commands.DeleteUser;
+using Tarker.Booking.Application.DataBase.User.Commands.UpdateUser;
+using Tarker.Booking.Application.DataBase.User.Commands.UpdateUserPassword;
+using Tarker.Booking.Application.DataBase.User.Queries.GetAllUser;
+using Tarker.Booking.Application.DataBase.User.Queries.GetUserById;
+using Tarker.Booking.Application.DataBase.User.Queries.GetUserByUserNameAndPassword;
 using Tarker.Booking.Application.Exceptions;
 using Tarker.Booking.Application.Features;
 
@@ -12,6 +19,99 @@ namespace Tarker.Booking.Api.Controllers
     {
         public UserController()
         {
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(
+            [FromBody] CreateUserModel model,
+            [FromServices] ICreateUserCommand createUserCommand)
+        {
+            var data = await createUserCommand.Execute(model);
+            return StatusCode(StatusCodes.Status201Created, 
+                ResponseApiService.Response(StatusCodes.Status201Created, data));
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(
+            [FromBody] UpdateUserModel model,
+            [FromServices] IUpdateUserCommand updateUserCommand)
+        {
+            var data = await updateUserCommand.Execute(model);
+            return StatusCode(StatusCodes.Status200OK, 
+                ResponseApiService.Response(StatusCodes.Status200OK, data, "Usuario actualizado exitosamente"));
+        }
+
+        [HttpPut("update-password")]
+        public async Task<IActionResult> UpdatePassword(
+            [FromBody] UpdateUserPasswordModel model,
+            [FromServices] IUpdateUserPasswordCommand updateUserPasswordCommand)
+        {
+            var data = await updateUserPasswordCommand.Execute(model);
+            return StatusCode(StatusCodes.Status200OK, 
+                ResponseApiService.Response(StatusCodes.Status200OK, data, "Contraseña actualizada exitosamente"));
+        }
+
+        [HttpDelete("delete/{userId}")]
+        public async Task<IActionResult> Delete(
+            int userId,
+            [FromServices] IDeleteUserCommand deleteUserCommand)
+        {
+            if (userId == 0)
+                return StatusCode(StatusCodes.Status400BadRequest, 
+                    ResponseApiService.Response(StatusCodes.Status400BadRequest));
+            var data = await deleteUserCommand.Execute(userId);
+
+            if (!data)
+                return StatusCode(StatusCodes.Status404NotFound, 
+                    ResponseApiService.Response(StatusCodes.Status404NotFound, data, "Usuario no encontrado"));
+
+            return StatusCode(StatusCodes.Status200OK, 
+                ResponseApiService.Response(StatusCodes.Status200OK, data, "Usuario eliminado exitosamente"));
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAll([FromServices] IGetAllUserQuery getAllUserQuery)
+        {
+            var data = await getAllUserQuery.Execute();
+            if(data == null || data.Count == 0)
+                return StatusCode(StatusCodes.Status404NotFound,
+                   ResponseApiService.Response(StatusCodes.Status404NotFound));
+
+            return StatusCode(StatusCodes.Status200OK,
+                ResponseApiService.Response(StatusCodes.Status200OK, data));
+        }
+
+        [HttpGet("get/{userId}")]
+        public async Task<IActionResult> GetById(
+            int userId,
+            [FromServices] IGetUserByIdQuery getUserByIdQuery)
+        {
+            if (userId == 0) 
+                return StatusCode(StatusCodes.Status400BadRequest,
+                    ResponseApiService.Response(StatusCodes.Status400BadRequest));
+
+            var data = await getUserByIdQuery.Execute(userId);
+            if (data == null)
+                return StatusCode(StatusCodes.Status404NotFound,
+                   ResponseApiService.Response(StatusCodes.Status404NotFound));
+
+            return StatusCode(StatusCodes.Status200OK,
+                ResponseApiService.Response(StatusCodes.Status200OK, data));
+        }
+
+        [HttpGet("get-by-username-password/{userName}/{password}")]
+        public async Task<IActionResult> GetByUserNamePassword(
+            string userName,
+            string password,
+            [FromServices] IGetUserByUserNameAndPasswordQuery getUserByUserNameAndPasswordQuery)
+        {
+            var data = await getUserByUserNameAndPasswordQuery.Execute(userName, password);
+            if (data == null)
+                return StatusCode(StatusCodes.Status404NotFound,
+                   ResponseApiService.Response(StatusCodes.Status404NotFound));
+
+            return StatusCode(StatusCodes.Status200OK,
+                ResponseApiService.Response(StatusCodes.Status200OK, data));
         }
     }
 }
